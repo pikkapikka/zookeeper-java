becd509e0dd624d06c17fc7c270c3765d0d142ca
chinaliwx <chinaliwx@163.com>
2016-07-14 11:59:12 +0800

    报采集平台内部错误时，锁定当前交易的机器人

--- curator-utils/src/test/java/Test.java
+++ curator-utils/src/test/java/Test.java
@@ -3,7 +3,6 @@ import com.softisland.curator.framework.BotCuratorUtils;
 import org.apache.curator.framework.CuratorFramework;
 
 import java.util.List;
-import java.util.Random;
 
 /**
  * Created by liwx on 2016/4/25.
@@ -12,88 +11,71 @@ public class Test {
 
     public static void main(String[] args) throws Exception{
         //123.57.53.128
+        //测试
         //String str = "172.16.14.84:2181,172.16.14.84:2182,172.16.14.84:2183";
+        //交易
         String str = "123.57.53.128:2181,123.57.53.128:2182,123.57.53.128:2183";
+        //轮盘
+        //String str = "158.85.190.88:2181,158.85.190.88:2182,158.85.190.88:2183";
+        //菠菜
+        //String str = "120.25.67.161:2181,120.25.67.161:2182,120.25.67.161:2183";
+        //国际
+        //String str = "47.88.103.138:2181,47.88.103.138:2182,47.88.103.138:2183";
         CuratorFramework client = CuratorFrameworkUtils.createSimple(str);
         client.start();
         /*CuratorCrudUtils.create(client,"/collect",null);
         CuratorCrudUtils.create(client,"/collect/bot",null);
-        CuratorCrudUtils.create(client,"/collect/bot/common",null);*/
+        CuratorCrudUtils.create(client,"/collect/bot/common",null);
         //collect/bot/vip
-        /*CuratorCrudUtils.create(client,"/collect/bot/vip",null);
+        CuratorCrudUtils.create(client,"/collect/bot/vip",null);
         CuratorCrudUtils.create(client,"/collect/bot/self",null);
         CuratorCrudUtils.create(client,"/collect/bot/used",null);
         CuratorCrudUtils.create(client,"/collect/bot/off",null);
-        CuratorCrudUtils.create(client,"/collect/bot/all",null);*/
+        CuratorCrudUtils.create(client,"/collect/bot/all",null);
 
-        /*CuratorCrudUtils.create(client,"/collect/bot/session",null);
-        CuratorCrudUtils.create(client,"/collect/bot/info",null);*/
+        CuratorCrudUtils.create(client,"/collect/bot/error",null);
 
-        //CuratorCrudUtils.create(client,"/collect/bot/offflag","false".getBytes());
+        //BotCuratorUtils.setTradeOffFlag(client,false);
+
+        CuratorCrudUtils.create(client,"/collect/bot/session",null);
+        CuratorCrudUtils.create(client,"/collect/bot/info",null);
+
+        CuratorCrudUtils.create(client,"/collect/bot/offflag","false".getBytes());
+
+        CuratorCrudUtils.create(client,"/collect/bot/wholesale",null);
+
 
         //addCommonBot(client,"123");
 
-       /* CuratorCrudUtils.create(client,"/collect/bot/stocknum",null);
+        CuratorCrudUtils.create(client,"/collect/bot/real",null);
+        CuratorCrudUtils.create(client,"/collect/bot/real/concurrent",null);
+
+        CuratorCrudUtils.create(client,"/collect/bot/stock",null);
+        CuratorCrudUtils.create(client,"/collect/bot/stock/lock",null);
+
+        CuratorCrudUtils.create(client,"/collect/bot/concurrent",null);
+
+        CuratorCrudUtils.create(client,"/collect/bot/stocknum",null);
         CuratorCrudUtils.create(client,"/collect/bot/stocknum/concurrent",null);*/
 
-        /*CuratorCrudUtils.create(client,"/collect/bot/stock",null);
-        CuratorCrudUtils.create(client,"/collect/bot/stock/lock",null);*/
-
-
-        //String flag = CuratorCrudUtils.getData(client,"/collect/bot/offflag");
         //BotCuratorUtils.setTradeOffFlag(client,false);
         //System.out.println(flag);
 
-
-        List<String> nodes =  BotCuratorUtils.getAllUsedBots(client);
-        System.out.println(nodes.size());
-
-        /*BotCuratorUtils.addOffLineBot(client,"76561198297106581","10.172.135.182;a");
-        BotCuratorUtils.addOffLineBot(client,"76561198297108021","10.172.135.182;a");
-        BotCuratorUtils.addOffLineBot(client,"76561198297678052","10.172.135.182;a");
-        BotCuratorUtils.addOffLineBot(client,"76561198299187591","10.172.135.182;a");
-        BotCuratorUtils.addOffLineBot(client,"76561198297806326","10.172.135.182;a");
-        BotCuratorUtils.addOffLineBot(client,"76561198297806457","10.172.135.182;a");
-        BotCuratorUtils.addOffLineBot(client,"76561198297302746","10.172.135.182;a");
-        BotCuratorUtils.addOffLineBot(client,"76561198296564451","10.172.135.182;a");
-        BotCuratorUtils.addOffLineBot(client,"76561198296427694","10.172.135.182;a");
-        BotCuratorUtils.addOffLineBot(client,"76561198297171736","10.172.135.182;a");*/
-
-       /* BotCuratorUtils.addOffLineBot(client,"76561198297106581","10.172.135.182;a");
-
-        BotCuratorUtils.addOffLineBot(client,"76561198297108021","10.172.135.182;a");
-
-        BotCuratorUtils.addOffLineBot(client,"76561198297678052","10.172.135.182;a");
-
-        BotCuratorUtils.addOffLineBot(client,"76561198299187591","10.172.135.182;a");
-
-        BotCuratorUtils.addOffLineBot(client,"76561198297806326","10.172.135.182;a");
-
-        BotCuratorUtils.addOffLineBot(client,"76561198297806457","10.172.135.182;a");
-
-        BotCuratorUtils.addOffLineBot(client,"76561198297302746","10.172.135.182;a");
-
-        BotCuratorUtils.addOffLineBot(client,"76561198296564451","10.172.135.182;a");
-
-        BotCuratorUtils.addOffLineBot(client,"76561198296427694","10.172.135.182;a");
-
-        BotCuratorUtils.addOffLineBot(client,"76561198297171736","10.172.135.182;a");*/
-
-
-        //BotCuratorUtils.addOffLineBot(client,"76561198297678052","10.172.135.182;a");
+        List<String> nodes =  BotCuratorUtils.getAllOffLineBots(client);
 
         //Map<String,String> botInfoMap = BotCuratorUtils.getAllOnlineBotsData(client);
 
         //BotCuratorUtils.deleUsedBot(client,"76561198297756834");
         nodes.forEach(v-> {
-            System.out.println(v);
             try {
-                //System.out.println(BotCuratorUtils.getOffLineBotData(client,v));
+                System.out.println(v);
+                //System.out.println(CuratorCrudUtils.getData(client,BotCuratorUtils.VIP_BOT_NODE_PATH+"/"+v));
             } catch (Exception e) {
                 e.printStackTrace();
             }
+            //76561198287312700
             /*try {
-                BotCuratorUtils.getOffLineBotData(client,v);
+                System.out.println(BotCuratorUtils.getOffLineBotData(client,v));
             } catch (Exception e) {
                 e.printStackTrace();
             }*/
@@ -131,10 +113,11 @@ public class Test {
                 e.printStackTrace();
             }*/
         });
+        System.out.println(nodes.size());
         /*botInfoMap.forEach((v,k)->{
-            System.out.println(v);
             try {
-                //BotCuratorUtils.deleBotInfo(client,v);
+                System.out.println("online:"+v);
+                BotCuratorUtils.deleBotSession(client,v);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -144,14 +127,5 @@ public class Test {
         client.close();
     }
 
-    public static void addCommonBot(CuratorFramework client,String id)throws Exception{
-        //BotCuratorUtils.addCommonBot(client,id,"172.16.14.65:8080");
-        BotCuratorUtils.deleCommonBot(client,id);
-    }
-
-    public static void main1(String[] args) {
-        Random random = new Random();
-        System.out.println(random.nextInt(0));
-    }
 
 }
